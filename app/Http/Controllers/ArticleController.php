e5c9b0b929602a162fffb9d8b06dd24d5dd487ba
Dwain <dwaincontact@gmail.com>
2025-12-17 10:55:28 +0100

    exo 9

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,7 +6,10 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function show($id): string{
-        return "Article n°$id.";
+    public function show($id)
+    {
+        return view('pages.article-details', [
+            'id' => $id
+        ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+@section('title', 'Accueil')
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
-    </head>
-    <body>
-                <h1>Bienvenue sur le site de {{$name}}</h1>
-    </body>
-</html>
+@section('content')
+    <h2>Bienvenue sur la page d’accueil</h2>
+    <p>Ceci est le contenu principal de la page.</p>
+@endsection
