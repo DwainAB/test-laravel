c020b86a4e77d7636ccd58b96e80be0711c7cf91
Dwain <dwaincontact@gmail.com>
2025-12-17 11:28:22 +0100

    exo 10

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="UTF-8">
-        <title>@yield('title')</title>
-    </head>
-    <body>
-        <header>
-            <h1>Mon site Laravel</h1>
-        </header>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+</head>
+<body>
 
-        <main>
-            @yield('content')
-        </main>
+    @include('partials.header')
 
-        <footer>
-            <p>© 2025 - Tous droits réservés</p>
-        </footer>
-    </body>
+    <main>
+        @yield('content')
+    </main>
+
+    @include('partials.footer')
+
+</body>
 </html>
