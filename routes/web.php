a65858995d9041daba67ab07dfd3e1950171103b
Dwain <dwaincontact@gmail.com>
2025-12-17 10:28:17 +0100

    Ajout page erreur

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,4 @@ use App\Http\Controllers\ArticleController;
 
 Route::get('/', [IndexController::class, 'index'])->name('home');
 Route::get('/article/{id}', [ArticleController::class, 'show'])->name('article.details');
-
+Route::fallback(function () { return response() ->view('errors.not-found', [], 404)});
